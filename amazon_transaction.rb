7ea33aa9e502d95d1e8ae359f3a1e85db9ee9a5b
hgrobel <hmgrobel@gmail.com>
2018-07-16 14:50:52 +0000

    automatically backed up by learn

--- amazon_transaction.rb
+++ amazon_transaction.rb
@@ -1,1 +1,24 @@
 # Code your cash register here!
+
+class AmazonTransaction
+  def initialize(total, items, discount = 20)
+    @total = 0 
+    @items = {}
+    @discount 
+  end
+  def total
+    
+end
+
+
+items = {"eggs" => 0.98, "books" => 5, "Lucky Charms" => 4.50, "Ritz Crackers" => 5.0, "Justin's Peanut Butter Cups" => 2.50}
+
+puts "What do you want to buy?"
+puts items
+answer = gets.chomp
+puts total
+
+
+
+
+
